ae7aa8de3f608f80531ff4ef8b39264ead6c0aac
Salavat <asnsalavat@gmail.com>
2021-11-20 13:33:46 +0100

    Default params

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,25 +1,16 @@
 ﻿#include <iostream>
 using namespace std; // Пространство имён - пока что не знаю, что это
 
-
-// Я хз зачем я это пишу, я это итак знаю - область определения переменных (глобальная и локальная)
-int aGlobal = 0;
-
-int method() {
-    return ++aGlobal;
-}
-
-int method(int a) {
-    return ++a;
-}
+// Разобрался с объявлением дефолтных значений в С++
+void defaultParams(int a, int b = 5);
 
 int main()
 {
-    int aLocal = 0;
-    cout << aGlobal;
-    cout << method();
-    cout << method(aLocal);
-    cout << method(aGlobal);
-
+    defaultParams(1);
+    defaultParams(3, 4);
     return 0;
+}
+
+void defaultParams(int a, int b) {
+    cout << a << " " << b << endl;
 }
