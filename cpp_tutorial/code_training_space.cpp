7c56c9543193fb74b43fc74fd5fd394692715b1b
Salavat <asnsalavat@gmail.com>
2021-11-20 13:11:44 +0100

    How prototypes work?

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,19 +1,33 @@
 ﻿#include <iostream>
 using namespace std; // Пространство имён - пока что не знаю, что это
 
-
-void print() {
-    cout << "HEllo" << endl;
-}
-
-void print(string s) {
-    cout << s << endl;
-}
+// Прототипы - нужны, чтобы объявить их в начале, а логику написать в нужном месте
+void prototype1();
+void prototype2();
+void prototype3();
+void prototype4();
 
 int main()
 {
-    print();  // 1
-    print("byee"); // перегрузка
+    prototype1();
+    prototype4();
+    prototype3();
+    prototype2();
     return 0;
 }
- // Я понял как работает git commit и когда git видит изменения.
+
+void prototype1() {
+    cout << "1" << endl;
+}
+
+void prototype2() {
+    cout << "2" << endl;
+}
+
+void prototype3() {
+    cout << "3" << endl;
+}
+
+void prototype4() {
+    cout << "4" << endl;
+}
