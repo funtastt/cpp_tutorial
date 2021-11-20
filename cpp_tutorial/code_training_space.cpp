a0734f1281141788143ca84636b485793563b23f
Salavat <asnsalavat@gmail.com>
2021-11-20 13:02:30 +0100

    overload

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,14 +1,33 @@
 ﻿#include <iostream>
 using namespace std; // Пространство имён - пока что не знаю, что это
 
-void printHello() {
+
+// Такой вариант работает - когда printHello() объявлен перед main()
+void print() {
     cout << "HEllo" << endl;
+}
+
+void print(string s) {
+    cout << s << endl;
 }
 
 int main()
 {
     cout << "Ok" << endl;
-    printHello();
+    print();
+    print("bye");
     return 0;
 }
+
+// А такой выдаёт ошибку С3861
+//int main()
+//{
+//    cout << "Ok" << endl;
+//    printHello();
+//    return 0;
+//}
+// 
+// void printHello() {
+// cout << "HEllo" << endl;
+// }
 
