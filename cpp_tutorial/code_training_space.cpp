1ca0acbc164c7bceb6cf0c7121869786e9de4a12
Salavat <asnsalavat@gmail.com>
2021-11-20 13:17:09 +0100

    Field of view

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,33 +1,25 @@
 ﻿#include <iostream>
 using namespace std; // Пространство имён - пока что не знаю, что это
 
-// Прототипы - нужны, чтобы объявить их в начале, а логику написать в нужном месте
-void prototype1();
-void prototype2();
-void prototype3();
-void prototype4();
+
+// Я хз зачем я это пишу, я это итак знаю - область определения переменных (глобальная и локальная)
+int aGlobal = 0;
+
+int method() {
+    return ++aGlobal;
+}
+
+int method(int a) {
+    return ++a;
+}
 
 int main()
 {
-    prototype1();
-    prototype4();
-    prototype3();
-    prototype2();
+    int aLocal = 0;
+    cout << aGlobal;
+    cout << method();
+    cout << method(aLocal);
+    cout << method(aGlobal);
+
     return 0;
-}
-
-void prototype1() {
-    cout << "1" << endl;
-}
-
-void prototype2() {
-    cout << "2" << endl;
-}
-
-void prototype3() {
-    cout << "3" << endl;
-}
-
-void prototype4() {
-    cout << "4" << endl;
 }
