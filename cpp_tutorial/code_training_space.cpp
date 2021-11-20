82f8e5ae67ca8cea6f0c9f1ee1e514febfe58f6c
Salavat <asnsalavat@gmail.com>
2021-11-20 12:59:07 +0100

    functions simple

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,17 +1,14 @@
 ﻿#include <iostream>
 using namespace std; // Пространство имён - пока что не знаю, что это
 
+void printHello() {
+    cout << "HEllo" << endl;
+}
+
 int main()
 {
-    int arr[5]{ 6,3,4,5,2};
-
-    int min = INT_MAX;
-
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
-        if (arr[i] < min)
-        {
-            min = arr[i]; 
-        }
-    }
-    cout << min;
+    cout << "Ok" << endl;
+    printHello();
+    return 0;
 }
+
