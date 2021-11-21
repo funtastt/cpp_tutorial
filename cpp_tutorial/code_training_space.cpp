01f65455824f7c500d8b8f38f3fc5362f9480622
Salavat <asnsalavat@gmail.com>
2021-11-21 10:12:21 +0100

    Swap

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,27 +1,22 @@
 ﻿#include <iostream>
 using namespace std; // Пространство имён - пока что не знаю, что это
 
-
-void swap(int *pa, int *pb) {
-	int swap = *pa;
-	*pa = *pb;
-	*pb = swap;
+void swap(int *a, int *b) {
+	int swap = *a;
+	*a = *b;
+	*b = swap;
 }
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	int plus, minus, divide, multiply;
 	int a, b;
+	cout << "1 число: ";
+	cin >> a;
+	cout << "2 число: ";
+	cin >> b;
 
-	cout << "Введите первое число: ";
-	cin  >> a;
-	cout << "Введите второе число: ";
-	cin  >> b;
+	swap(&a, &b);
 
-	swap(a, b);
-
-	cout << a << endl;
-	cout << b << endl;
-
-	return 0;
+	cout << "\n1 число: " << a << endl;
+	cout << "2 число: " << b << endl;
 }
