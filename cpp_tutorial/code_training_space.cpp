c205e747d321cd770be3c6aa10a4e4231d9fd890
Salavat <asnsalavat@gmail.com>
2021-11-21 22:26:55 +0100

    Error - can't delete object Student normally

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -4,68 +4,38 @@ using namespace std;
 
 class Student {
 private:
-    string name, faculty;
-    int age, course;
     string lessons[2] = {string("lesson1"), string("lesson2")};
 
 public:
     Student() {
-        name = "Anonymous";
-        faculty = "Not selected";
-        age = 0;
-        course = 0;
+        
     }
-    Student(string setName, string setFaculty, int setAge, int setCourse) {
-        name = setName;
-        faculty = setFaculty;
-        age = setAge;
-        course = setCourse;
-    }
-
-    // Деструктор - антипод конструктору - вызывается при удалении объекта из памяти.
-    // В Java этого нет т.к. там есть автоматический сборщик мусора.
 
     ~Student() {
         delete lessons;
     }
 
-    string getName() {
-        return name;
+    string* getLessons() {
+        return lessons;
     }
-    string getFaculty() {
-        return faculty;
-    }
-    int getAge() {
-        return age;
-    }
-    int getCourse() {
-        return course;
-    }
-    void showInfo() {
-        cout << "Имя: " << getName() << endl;
-        cout << "Факультет: " << getFaculty() << endl;
-        cout << "Возраст: " << getAge() << endl;
-        cout << "Курс: " << getCourse() << endl;
-
-        cout << "Элективы: ";
-        int size = sizeof(lessons) / sizeof(string);
-        for (int i = 0; i < size; i++)
-        {
-            cout << lessons[i] << "\t";
-        }
-    } 
 };
+
+ostream& operator<<(std::ostream& strm, Student student) {
+    return strm << "Уроки: ";
+    string* lessons = student.getLessons();
+    
+    for (int i = 0; i < 2; i++)
+    {
+        cout << lessons[i] << endl;
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    Student *student = new Student("Salavat", "МГУ ВМК", 22, 1);
+    Student *student = new Student();
     
-    student->showInfo();
-     
-    if (student)
-    {
-        delete student;
-    }
+    cout << student << endl;
+
     return 0;
 }
