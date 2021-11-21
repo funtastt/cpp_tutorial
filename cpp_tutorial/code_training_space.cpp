81f06aee8ce14f701874fcd900aadd276ed1b969
Salavat <asnsalavat@gmail.com>
2021-11-21 20:06:34 +0100

    Destructor, strange String initialization, default constructor etc

--- cpp_tutorial/code_training_space.cpp
+++ cpp_tutorial/code_training_space.cpp
@@ -1,29 +1,71 @@
 ﻿#include <iostream>
 #include <string>
-#define getPlus(a,b) ((a)+(b))
-#define getMinus(a,b) ((a) - (b))
-#define getMultiply(a,b) ((a) * (b))
-#define getDivide(a,b) ((a) / (b))
-using namespace std; // Пространство имён - пока что не знаю, что это
-using namespace std;
+using namespace std; 
 
-// Указатель на функцию - прикольная штука.
-void calculate(int a, int b, int (*method)(int a, int b)) {
-    cout << "Результат: " << method(a, b) << endl;
-}
+class Student {
+private:
+    string name, faculty;
+    int age, course;
+    string lessons[2] = {string("lesson1"), string("lesson2")};
 
+public:
+    Student() {
+        name = "Anonymous";
+        faculty = "Not selected";
+        age = 0;
+        course = 0;
+    }
+    Student(string setName, string setFaculty, int setAge, int setCourse) {
+        name = setName;
+        faculty = setFaculty;
+        age = setAge;
+        course = setCourse;
+    }
+
+    // Деструктор - антипод конструктору - вызывается при удалении объекта из памяти.
+    // В Java этого нет т.к. там есть автоматический сборщик мусора.
+
+    ~Student() {
+        delete lessons;
+    }
+
+    string getName() {
+        return name;
+    }
+    string getFaculty() {
+        return faculty;
+    }
+    int getAge() {
+        return age;
+    }
+    int getCourse() {
+        return course;
+    }
+    void showInfo() {
+        cout << "Имя: " << getName() << endl;
+        cout << "Факультет: " << getFaculty() << endl;
+        cout << "Возраст: " << getAge() << endl;
+        cout << "Курс: " << getCourse() << endl;
+
+        cout << "Элективы: ";
+        int size = sizeof(lessons) / sizeof(string);
+        for (int i = 0; i < size; i++)
+        {
+            cout << lessons[i] << "\t";
+        }
+    } 
+};
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    int a = 123;
-    int b = 3;
-
-    cout << getPlus(a, b) << endl;
-    cout << getMinus(a, b) << endl;
-    cout << getMultiply(a, b) << endl;
-    cout << getDivide(a, b) << endl;
-
-
+    Student *student = new Student("Salavat", "МГУ ВМК", 22, 1);
+    
+    student->showInfo();
+     
+    if (student)
+    {
+        delete student;
+    }
     return 0;
 }
